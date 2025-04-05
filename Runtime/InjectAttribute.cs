bebbc005ca401c15314203820a4c8128bed37f9f
d.bogdanov <d.bodgdanov@nexters.com>
2025-04-05 19:19:25 +0400

    fix warnings

--- Runtime/InjectAttribute.cs
+++ Runtime/InjectAttribute.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using JetBrains.Annotations;
 
 namespace LightDI.Runtime
 {
+[MeansImplicitUse]
 [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Field)]
 public class InjectAttribute : Attribute
 {
